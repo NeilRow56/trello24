32fd7d5621416ebbacbd883e36ea8726368d6c5a
NeilRow56 <neilrowland56@gmail.com>
2023-12-22 07:04:53 +0000

    Added Clerk authentication

--- src/app/(marketing)/_components/Navbar.tsx
+++ src/app/(marketing)/_components/Navbar.tsx
@@ -1,22 +1,31 @@
+'use client'
+
 import Link from 'next/link'
 
 import { Button } from '@/components/ui/button'
 import { Logo } from '@/components/Logo'
 
+import { ClerkProvider, UserButton } from '@clerk/nextjs'
+
 export const Navbar = () => {
   return (
-    <div className="fixed top-0 flex h-14 w-full items-center border-b bg-white px-4 shadow-sm">
-      <div className="mx-auto flex w-full items-center justify-between md:max-w-screen-2xl">
-        <Logo />
-        <div className="flex w-full items-center justify-between space-x-4 md:block md:w-auto">
-          <Button size="sm" variant="outline" asChild>
-            <Link href="/sign-in">Login</Link>
-          </Button>
-          <Button size="sm" asChild>
-            <Link href="/sign-up">Get Taskify for free</Link>
-          </Button>
+    <ClerkProvider>
+      <div className="fixed top-0 flex h-14 w-full items-center border-b bg-white px-4 shadow-sm">
+        <div className="mx-auto flex w-full items-center justify-between md:max-w-screen-2xl">
+          <Logo />
+          <div className="flex gap-4">
+            <div className="flex w-full flex-row items-center justify-between space-x-4 md:block md:w-auto">
+              <Button size="sm" variant="outline" asChild>
+                <Link href="/sign-in">Login</Link>
+              </Button>
+              <Button size="sm" asChild>
+                <Link href="/sign-up">Get Taskify for free</Link>
+              </Button>
+            </div>
+            <UserButton afterSignOutUrl="/" />
+          </div>
         </div>
       </div>
-    </div>
+    </ClerkProvider>
   )
 }
