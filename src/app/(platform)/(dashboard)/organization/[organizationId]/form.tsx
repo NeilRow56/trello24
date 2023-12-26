a26a8aea362056aa77cdf9082095f4aa3569d4f6
NeilRow56 <neilrowland56@gmail.com>
2023-12-26 06:59:48 +0000

    Added Update to form.tsx for useActions hook etc;

--- src/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,18 +1,29 @@
 'use client'
 
-import { State, create } from '@/actions/create-board'
-import { Button } from '@/components/ui/button'
-import { useFormState } from '../../../../../../react-dom-shim'
+import { createBoard } from '@/actions/create-board'
+
 import FormInput from './form-input'
 import FormButton from './form-button'
+import { useAction } from '@/hooks/use-actions'
 
 const Form = () => {
-  const initialState = { message: null, errors: {} }
-  const [state, dispatch] = useFormState<State, FormData>(create, initialState)
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log(data, 'SUCCESS')
+    },
+    onError: (error) => {
+      console.error(error)
+    },
+  })
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string
+
+    execute({ title })
+  }
   return (
-    <form action={dispatch}>
+    <form action={onSubmit}>
       <div className="flex flex-col space-y-2">
-        <FormInput errors={state?.errors} />
+        <FormInput errors={fieldErrors} />
       </div>
 
       <FormButton />
