b82a72ac1a2cc7ca9edea09736beb628f080d487
NeilRow56 <neilrowland56@gmail.com>
2023-12-27 10:54:50 +0000

    Added unsplash images to form-picker

--- src/components/form/form-popover.tsx
+++ src/components/form/form-popover.tsx
@@ -16,6 +16,7 @@ import { Button } from '../ui/button'
 
 import { useAction } from '@/hooks/use-actions'
 import { createBoard } from '@/actions/create-board'
+import FormPicker from './form-picker'
 
 interface FormPopoverProps {
   children: React.ReactNode
@@ -67,6 +68,7 @@ export const FormPopover = ({
         </PopoverClose>
         <form action={onSubmit} className="space-y-4">
           <div className="space-y-4">
+            <FormPicker id="image" errors={fieldErrors} />
             <FormInput
               id="title"
               label="Board title"
