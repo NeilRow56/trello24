ff4f637b79de453c72ebbc34242fca18d8708379
NeilRow56 <neilrowland56@gmail.com>
2023-12-28 05:50:52 +0000

    Updated BoardList component to displat all boards for each organisation

--- src/app/(platform)/(dashboard)/organization/[organizationId]/_components/BoardList.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/_components/BoardList.tsx
@@ -1,11 +1,29 @@
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import { HelpCircle, User2 } from 'lucide-react'
+
+import { db } from '@/lib/db'
 import { Hint } from '@/components/Hint'
 import { FormPopover } from '@/components/form/form-popover'
 import { Skeleton } from '@/components/ui/skeleton'
-import { HelpCircle, User2 } from 'lucide-react'
+import Link from 'next/link'
 
-type Props = {}
+const BoardList = async () => {
+  const { orgId } = auth()
 
-const BoardList = (props: Props) => {
+  if (!orgId) {
+    return redirect('/select-org')
+  }
+
+  const boards = await db.board.findMany({
+    where: {
+      orgId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
   return (
     <div className="space-y-4">
       <div className="flex items-center text-lg font-semibold text-neutral-700">
@@ -13,6 +31,17 @@ const BoardList = (props: Props) => {
         Your boards
       </div>
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
+        {boards.map((board) => (
+          <Link
+            key={board.id}
+            href={`/board/${board.id}`}
+            className="group relative aspect-video h-full w-full overflow-hidden rounded-sm bg-sky-700 bg-cover bg-center bg-no-repeat p-2"
+            style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
+          >
+            <div className="absolute inset-0 bg-black/30 transition group-hover:bg-black/40" />
+            <p className="relative font-semibold text-white">{board.title}</p>
+          </Link>
+        ))}
         <FormPopover sideOffset={10} side="right">
           <div
             role="button"
