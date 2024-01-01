df1c5a075f5ffba3bfff720c86c2803cc687e40d
NeilRow56 <neilrowland56@gmail.com>
2024-01-01 17:09:21 +0000

    Added CardItem.tsx to ListItem.tsx

--- src/app/(platform)/(dashboard)/board/[boardId]/_components/ListItem.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/ListItem.tsx
@@ -4,6 +4,8 @@ import { ElementRef, useRef, useState } from 'react'
 import { ListWithCards } from '../../../../../../../types'
 import ListHeader from './ListHeader'
 import { CardForm } from './CardForm'
+import { cn } from '@/lib/utils'
+import { CardItem } from './CardItem'
 
 type ListItemProps = {
   index: number
@@ -28,6 +30,16 @@ const ListItem = ({ index, data }: ListItemProps) => {
     <li className="delect-none h-full w-[272px] shrink-0">
       <div className="w-full rounded-md bg-[#f1f2f4] pb-2 shadow-md">
         <ListHeader onAddCard={enableEditing} data={data} />
+        <ol
+          className={cn(
+            'mx-1 flex flex-col gap-y-2 px-1 py-0.5',
+            data.cards.length > 0 ? 'mt-2' : 'mt-0'
+          )}
+        >
+          {data.cards.map((card, index) => (
+            <CardItem index={index} key={card.id} data={card} />
+          ))}
+        </ol>
         <CardForm
           listId={data.id}
           ref={textareaRef}
